a4be4517c638bc905a5f7db779d9960ee4522ac4
Piyush Shukla <piyusshukla59@gmail.com>
2026-02-04 09:35:01 +0000

    Conversion 0f data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+//   let Grade = 33 // o/p: -> number
+
+//   console.log(typeof Grade);
+
+// let score = true  // o/p: -> String
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // when "33abc" is the value of score --> string => console.log(valueInNumber); --> we get in outcome NaN
+// /*
+// // when null is the value of score --> null => console.log(valueInNumber); --> we get in outcome 0
+// // when undefined is the value of score --> undefined => console.log(valueInNumber); --> we get in outcome NaN
+// // when true/false is the value of score --> true/false => console.log(valueInNumber); --> we get in outcome 1(true)/0(false)
+
+// */
+
+// // "33" => 33
+// // "33123" => NaN
+// // " " => false
+// // "piyush" => true
+
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
