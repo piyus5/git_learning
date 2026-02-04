1d0bdac54a20d79c8440386ea12c36af54ce0636
Piyush Shukla <piyusshukla59@gmail.com>
2026-02-04 11:56:02 +0000

    Operation Practice

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,8 +27,39 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ****************** Operation *******************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(5-2);
+// console.log(2*2);
+// console.log(2**4); // 2*2*2*2= 16
+// console.log(10/2);
+// console.log(9%2);
+
+// let str1 = "hello";
+// let str2 = " Piyush";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2 + 2); // --> 122
+// console.log(1 + "2"); // 12
+// console.log("1" + "2"); // 12
+// console.log(1+ 2 + "2"); // 32
+// console.log((3 + 4)* 5 % 3); // 7*5=35 -> 35%3=2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); // 101
+gameCounter++;
+console.log(gameCounter); // 102
+
