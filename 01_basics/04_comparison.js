20e248ae69a628c51d08ad7997a0186fd37569ea
Piyush Shukla <piyusshukla59@gmail.com>
2026-02-04 16:55:09 +0000

    learn about comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);  // true : even if there is different data type
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // reason: comparison converts null to a number, treating it as 0. that's why null >= 0 is true
+
+console.log(undefined == 0); //false
+
+console.log("3" === 3);  // it checks strictly as it compares as well as checks its data type
+
+
+
