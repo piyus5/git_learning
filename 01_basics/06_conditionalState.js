87bae0079998420b58dfdd3638e60929294f22c9
Piyush Shukla <piyusshukla59@gmail.com>
2026-02-06 08:49:52 +0000

    learn about conditional statements

---
+++ 01_basics/06_conditionalState.js
@@ -0,0 +1,26 @@
+let number = prompt("Enter a Number: ");
+if (number % 5 === 0){
+    console.log(number, "is multiple of 5");
+} else {
+    console.log(number, "is multiple of 5");
+}
+
+
+
+let score = 85;
+let grade;
+
+if(score >= 90 && score <= 100) {
+    grade = 'A';
+} else if (score  >= 70 && score <= 89) {
+    grade = 'B';
+} else if (score  >= 60 && score <= 69) {
+    grade = 'C';
+} else if (score  >= 50 && score <= 59) {
+    grade = 'D';
+} else if (score  >= 0 && score <= 49) {
+    grade = 'F';
+} 
+
+console.log("According to your scores, your grade is : ", grade);
+
